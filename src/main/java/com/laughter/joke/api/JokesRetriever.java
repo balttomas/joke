f0bc8612bb9a349c270281e2a9b12db743c7b7d8
Tomas Baltrukonis <tomas.baltrukonis@nortal.com>
2023-09-18 19:09:28 +0300

    externalize rest invocation to service

--- src/main/java/com/laughter/joke/api/JokesRetriever.java
+++ src/main/java/com/laughter/joke/api/JokesRetriever.java
@@ -1,33 +1,21 @@
 package com.laughter.joke.api;
 
-import com.laughter.joke.domain.Joke;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping(value = "/jokes", produces = MediaType.APPLICATION_JSON_VALUE)
 public class JokesRetriever {
 
   @Autowired
-  private RestTemplate restTemplate;
-
-  @Value("${api.get.random}")
-  private String jokeUrl;
+  private JokesClient jokesApi;
 
   @GetMapping(value = "/random")
-  public Joke findRandomJoke() {
-    try {
-      ResponseEntity<Joke> response = restTemplate.getForEntity(jokeUrl, Joke.class);
-      return response.getBody();
-    } catch (Exception e) {
-      throw new RuntimeException(e);
-    }
+  public String findRandomJoke() {
+    return jokesApi.findRandomJoke();
   }
 
 }

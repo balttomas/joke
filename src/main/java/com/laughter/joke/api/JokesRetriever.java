af30920020fb1675beaf75542cec131c27038e8e
Tomas Baltrukonis <tomas.baltrukonis@nortal.com>
2023-09-18 22:03:02 +0300

    wrap rest client with service

--- src/main/java/com/laughter/joke/api/JokesRetriever.java
+++ src/main/java/com/laughter/joke/api/JokesRetriever.java
@@ -1,5 +1,7 @@
 package com.laughter.joke.api;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,9 +15,11 @@ public class JokesRetriever {
   @Autowired
   private JokesClient jokesApi;
 
+  private ObjectMapper objectMapper = new ObjectMapper();
+
   @GetMapping(value = "/random")
-  public String findRandomJoke() {
-    return jokesApi.findRandomJoke();
+  public String findRandomJoke() throws JsonProcessingException {
+    return objectMapper.writeValueAsString(jokesApi.findRandomJoke());
   }
 
 }

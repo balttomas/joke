d44f3cda8182a4819e25f86e9026b417c0db1a7d
Tomas Baltrukonis <tomas.baltrukonis@nortal.com>
2023-09-24 12:44:30 +0300

    jokes middleman service implementation with rate limit configuration

--- src/main/java/com/laughter/joke/mediator/JokeService.java
+++ src/main/java/com/laughter/joke/mediator/JokeService.java
@@ -1,7 +1,11 @@
 package com.laughter.joke.mediator;
 
 import com.laughter.joke.base.ClientApi;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,17 +18,40 @@ public class JokeService implements JokesMediatorApi {
   private final ClientApi chuckNorrisClient;
 
   @Override
+  @RateLimiter(name = "jokes")
   public String findRandomJoke() {
     return chuckNorrisClient.findRandomJoke().getJokeContent().get(0);
   }
 
   @Override
+  @RateLimiter(name = "jokes")
   public String findRandomJokeByCategory(String category) {
     return chuckNorrisClient.findRandomJokeByCategory(category).getJokeContent().get(0);
   }
 
   @Override
+  @RateLimiter(name = "jokes")
   public List<String> findManyJokes(String query) {
-    return chuckNorrisClient.findManyJokes(query).getJokeContent();
+    List<String> jokes = chuckNorrisClient.findManyJokes(query).getJokeContent();
+
+/*    String lowerCaseQuery = query.toLowerCase();
+    String[] criteria = {" " + lowerCaseQuery, "," + lowerCaseQuery,
+        "?" + lowerCaseQuery, "." + lowerCaseQuery, "!" + lowerCaseQuery};
+    return jokes.stream()
+        .filter(joke -> StringUtils.indexOfAny(joke.toLowerCase(), criteria) > -1)
+        .toList();*/
+
+    return jokes.stream().filter(joke -> satisfiesQuery(joke, query)).toList();
+
+  }
+
+  private boolean satisfiesQuery(String joke, String query) {
+    var lowerCaseQuery = query.toLowerCase();
+    return Optional.of(joke)
+        .map(String::toLowerCase)
+        .map(jokeText -> jokeText.split("[, ?.!'\"\n\r]+"))
+        .map(jokesArray -> new HashSet<>(Arrays.asList(jokesArray)))
+        .map(jokesSet -> jokesSet.contains(lowerCaseQuery))
+        .orElse(false);
   }
 }
---
+++ src/test/java/com/laughter/joke/mediator/JokeServiceTest.java
@@ -0,0 +1,125 @@
+package com.laughter.joke.mediator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import com.laughter.joke.base.ClientApi;
+import com.laughter.joke.base.Joke;
+import com.laughter.joke.client.norris.MultipleNorrisJokes;
+import com.laughter.joke.client.norris.NorrisJoke;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+public class JokeServiceTest {
+
+  @InjectMocks
+  private JokeService service;
+
+  @Mock
+  private ClientApi chuckNorrisClient;
+
+  @Test
+  void shouldFindRandomSingleJokeByCategory() {
+    String jokeText = "Funny joke by category.";
+    String jokeCategory = "category";
+    given(chuckNorrisClient.findRandomJokeByCategory(jokeCategory)).willReturn(
+        NorrisJoke.builder().value(jokeText).build()
+    );
+
+    String result = service.findRandomJokeByCategory(jokeCategory);
+
+    verify(chuckNorrisClient, times(1)).findRandomJokeByCategory(jokeCategory);
+    assertThat(result)
+        .isNotBlank()
+        .isEqualTo(jokeText);
+  }
+
+  @Test
+  void shouldFindRandomSingleJoke() {
+    String jokeText = "Funny man. Joke.";
+    given(chuckNorrisClient.findRandomJoke()).willReturn(
+        NorrisJoke.builder().value(jokeText).build()
+    );
+
+    String result = service.findRandomJoke();
+
+    verify(chuckNorrisClient, times(1)).findRandomJoke();
+    assertThat(result)
+        .isNotBlank()
+        .isEqualTo(jokeText);
+  }
+
+  @Test
+  void shouldRetrieveMultipleJokesFilteredOutByQuery() {
+    String filterCriteria = "mAn";
+    given(chuckNorrisClient.findManyJokes(filterCriteria)).willReturn(createJokes());
+
+    List<String> result = service.findManyJokes(filterCriteria);
+
+    verify(chuckNorrisClient, times(1)).findManyJokes(filterCriteria);
+    assertThat(result)
+        .isNotNull()
+        .hasSize(6)
+        .containsExactlyInAnyOrder(
+            "Funny man. Joke.",
+            "Funny.maN. Joke.",
+            "Funny.man? Joke.",
+            "Funny.Man! Joke.",
+            """
+                Funny.man
+                fail Joke.""",
+            "Super man Joke."
+        );
+
+  }
+
+  @Test
+  void shouldNotRetrieveAnyJokesFilteredOutByQuery() {
+    String filterCriteria = "man";
+    given(chuckNorrisClient.findManyJokes(filterCriteria)).willReturn(
+        findJokesNotSatisfyingCriteria());
+
+    List<String> result = service.findManyJokes(filterCriteria);
+
+    assertThat(result)
+        .isNotNull()
+        .isEmpty();
+  }
+
+  private Joke findJokesNotSatisfyingCriteria() {
+    return MultipleNorrisJokes.builder()
+        .result(List.of(NorrisJoke.builder().value("Funnyman. Joke.").build(),
+            NorrisJoke.builder().value("manmanman Joke.").build(),
+            NorrisJoke.builder().value("Superman Joke.").build()))
+        .total(3).build();
+  }
+
+  private Joke createJokes() {
+    var thirdPartyJokes = List.of(
+        NorrisJoke.builder().value("Funny man. Joke.").build(),
+        NorrisJoke.builder().value("Funnyman. Joke.").build(),
+        NorrisJoke.builder().value("Funny.maN. Joke.").build(),
+        NorrisJoke.builder().value("Funny.man? Joke.").build(),
+        NorrisJoke.builder().value("Funny.Man! Joke.").build(),
+        NorrisJoke.builder().value("""
+            Funny.man
+            fail Joke.""").build(),
+        NorrisJoke.builder().value("manmanman Joke.").build(),
+        NorrisJoke.builder().value("Superman Joke.").build(),
+        NorrisJoke.builder().value("Super man Joke.").build()
+    );
+
+    return MultipleNorrisJokes.builder()
+        .result(thirdPartyJokes)
+        .total(thirdPartyJokes.size())
+        .build();
+  }
+
+}
